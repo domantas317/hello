ab60a567fb65b5617fca460221cf622330fb80a5
domantas317 <46021945+domantas317@users.noreply.github.com>
2019-01-03 20:37:09 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <html>
-<html lang="lt" dir="ltr">
+<html lang="eng" dir="ltr">
     <head>
         <link rel="stylesheet" href="styles.css">
         <meta charset="UTF-8">
         <title>Miami</title>
     </head>
     <body style="background-color:silver">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+    </ul>
 <h1 style="text-align:center">Welcome to my web page about miami</h1>
     <p>Miami, officially the City of Miami, is the cultural, economic and financial center of South Florida. Miami is the seat of Miami-Dade County, the most populous county in Florida. The city covers an area of about 56.6 square miles (147 km2), between the Everglades to the west and Biscayne Bay on the east; it is also the sixth most densely populated major city in the United States. The Miami metropolitan area is home to 6.1 million people and the seventh-largest metropolitan area in the nation. Miami's metro area is the second-most populous metropolis in the southeastern United States and fourth-largest urban area in the U.S.</p>
-<img src="City.jpg" alt="Picture of Miami" style="float:right; width:400px; height:400px;"
+<img src="City.jpg" alt="Picture of Miami">
     <br>
 <br>
-<p2>It is the second time I am editing this.</p2>
+<p>It is the second time I am editing this.</p>
     <!-- comment about the web page -->
 <p>The third <a href="https://www.w3schools.com/html/html_links.asp" target="_blank">time</a> is the charm.</p>
     </body>
