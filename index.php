f6b355148afe82095bfc8059f7fce73115766988
domantas317 <46021945+domantas317@users.noreply.github.com>
2019-01-31 20:16:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
         <link rel="stylesheet" type="text/css" href="styles.css">
         <meta charset="UTF-8">
         <title>Miami</title>
-        <style>
-            #header {
-                background-color: lightblue;
-                color: black;
-                padding: 30px;
-                text-align: left;
-                margin-top: 0px;
-            }
-        </style>
     </head>
     <body style="background-color:silver">
     <ul>
@@ -20,9 +11,17 @@
         <li><a href="#contact">Contact</a></li>
         <li><a href="#about">About</a></li>
     </ul>
-<h1 id="header">Welcome to my web page about miami</h1>
+    <div id="sidenav">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+<h1 id="table">Welcome to my web page about miami</h1>
     <p>Miami, officially the City of Miami, is the cultural, economic and financial center of South Florida. Miami is the seat of Miami-Dade County, the most populous county in Florida. The city covers an area of about 56.6 square miles (147 km2), between the Everglades to the west and Biscayne Bay on the east; it is also the sixth most densely populated major city in the United States. The Miami metropolitan area is home to 6.1 million people and the seventh-largest metropolitan area in the nation. Miami's metro area is the second-most populous metropolis in the southeastern United States and fourth-largest urban area in the U.S.</p>
-<img src="City.jpg" alt="Picture of Miami">
+    <div class="picture">
+    <img src="City.jpg" alt="Picture of Miami">
+    </div>
     <br>
 <br>
 <p>It is the second time I am editing this.</p>
