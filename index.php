8f8925a7a5aeea47cc6b83250ab28daebd0b7f31
v0lkas <vitalij.vladimirov@mistertango.com>
2019-02-11 12:42:55 +0200

    Margins corrected.

--- index.php
+++ index.php
@@ -19,14 +19,17 @@
         <li><a href="#">Contact</a></li>
     </ul>
     <div id="text">
-        <p><img src="City.jpg" alt="Picture of Miami" height="400" width="350" style="float:left">
+        <img src="City.jpg" alt="Picture of Miami" height="400" width="350" style="float:left">
+        <p class="content">
             Miami, officially the City of Miami, is the cultural, economic and financial center of
-            South Florida. <a href="https://www.w3schools.com/html/html_links.asp" target="_blank">Miami</a> is the seat of Miami-Dade County, the most populous county in Florida.
+            South Florida. <a href="https://www.w3schools.com/html/html_links.asp" target="_blank">Miami</a>
+            is the seat of Miami-Dade County, the most populous county in Florida.
             The city covers an area of about 56.6 square miles (147 km2), between the Everglades to the west and
             Biscayne Bay on the east; it is also the sixth most densely populated major city in the United States.
             The Miami metropolitan area is home to 6.1 million people and the seventh-largest metropolitan area in
             the nation. Miami's metro area is the second-most populous metropolis in the southeastern United States
-            and fourth-largest urban area in the U.S.</p>
+            and fourth-largest urban area in the U.S.
+        </p>
         <!-- comment about the web page -->
     </div>
     <br>
