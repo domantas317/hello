8420e24475c0f70217db3a4d907964007187f1ff
domantas317 <naulis.domantas@gmail.com>
2019-03-02 13:22:22 +0200

    Testing git and phpStorm

--- index.php
+++ index.php
@@ -11,16 +11,22 @@
         <li><a href="#contact">Contact</a></li>
         <li><a href="#about">About</a></li>
     </ul>
-<h1 style="text-align: center">Welcome to my web page about miami</h1>
-    <ul id="right_menu">
+<h1 style="text-align: center">The city of Miami</h1>
+    <ul id="right_menu" style="list-style: none">
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Clients</a></li>
         <li><a href="#">Contact</a></li>
     </ul>
+    <div id="beginning">
+    <p>The main purpose of this web page is to provide a bit of information about one of the biggest American cities Miami,
+        which is located at the south of USA. IF you will have any questions regarding this web page please contact me at
+    this adress: dsdksada.</p>
+    </div>
     <div id="text">
         <img src="City.jpg" alt="Picture of Miami" height="400" width="350" style="float:left">
         <p class="content">
+            <span style='font-size:30px;'>&#9733;&#9733;&#9733;&#9733;&#9733;</span><br>
             Miami, officially the City of Miami, is the cultural, economic and financial center of
             South Florida. <a href="https://www.w3schools.com/html/html_links.asp" target="_blank">Miami</a>
             is the seat of Miami-Dade County, the most populous county in Florida.
@@ -28,7 +34,19 @@
             Biscayne Bay on the east; it is also the sixth most densely populated major city in the United States.
             The Miami metropolitan area is home to 6.1 million people and the seventh-largest metropolitan area in
             the nation. Miami's metro area is the second-most populous metropolis in the southeastern United States
-            and fourth-largest urban area in the U.S.
+            and fourth-largest urban area in the U.S.<br>
+            <br>
+            Miami is a major center, and a leader in finance, commerce, culture, media, entertainment, the arts,
+            and international trade. The Miami Metropolitan Area is by far the largest urban economy in Florida and the
+            12th largest in the United States with a GDP of $344.9 billion as of 2017. In 2012, Miami was classified as
+            an "Alpha−" level world city in the World Cities Study Group's inventory.
+            In 2010, Miami ranked seventh in the United States and 33rd among global cities in terms of business activity,
+            human capital, information exchange, cultural experience, and political engagement.
+            In 2008, Forbes magazine ranked Miami "America's Cleanest City", for its year-round good air quality,
+            vast green spaces, clean drinking water, clean streets, and citywide recycling programs.
+            According to a 2009 UBS study of 73 world cities, Miami was ranked as the richest city in the United States,
+            and the world's seventh-richest city in terms of purchasing power. Miami is nicknamed the
+            "Capital of Latin America" and is the largest city with a Cuban-American plurality.
         </p>
         <!-- comment about the web page -->
     </div>
